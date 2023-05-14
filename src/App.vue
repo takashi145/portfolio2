2e8b217dbc8e2cd2e3363e31db38fe5249572b59
takashi145 <t.shuhei0203@gmail.com>
2023-05-15 02:15:49 +0900

    Create ToggleDarkMode.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
+import ToggleDarkMode from './components/ToggleDarkMode.vue';
 </script>
-
 <template>
-  <div class="">
-    aa
+  <div class="min-h-screen bg-gray-200 dark:bg-slate-800">
+    <div class="max-w-7xl mx-auto">
+
+      <!-- Toggle Dark Mode -->
+      <div class="text-end p-3">
+        <ToggleDarkMode />
+      </div>
+      
+      <div>
+
+      </div>
+    </div>
   </div>
 </template>
 
