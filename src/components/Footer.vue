10d00962e50d11495efd0c655070ec93ef429fa5
takashi145 <t.shuhei0203@gmail.com>
2023-05-15 04:55:07 +0900

    Update App.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+</script>
+<template>
+  <footer class="z-40 fxed bottom-0 w-full pb-2 bg-gray-200 border-t border-gray-300 dark:bg-slate-800 dark:border-slate-700">
+    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        © 2023 <a href="#" class="hover:underline">takashi145</a>. All Rights Reserved.
+      </span>
+      <!-- <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+          <li>
+              <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
+          </li>
+          <li>
+              <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+          </li>
+          <li>
+              <a href="#" class="mr-4 hover:underline md:mr-6">Licensing</a>
+          </li>
+          <li>
+              <a href="#" class="hover:underline">Contact</a>
+          </li>
+      </ul> -->
+    </div>
+  </footer>
+</template>
