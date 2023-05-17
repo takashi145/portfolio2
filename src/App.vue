0a5f5dd6911b468afbcdbd4c8a35a9e796cc56fd
takashi145 <t.shuhei0203@gmail.com>
2023-05-18 00:41:52 +0900

    Update App.vue,

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,7 @@ import skills from './data/skills.json';
 
       <div class="space-y-14">
         <!-- SKills -->
-        <div class="text-center mx-auto max-w-3xl">
+        <!-- <div class="text-center mx-auto max-w-3xl">
           <h2 class="flex items-center justify-center text-2xl font-semibold text-gray-800 mb-4 dark:text-white">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-7 h-7 mr-2">
               <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
@@ -42,7 +42,7 @@ import skills from './data/skills.json';
           >
             #{{ skill.name }}
           </div>
-        </div>
+        </div> -->
 
         <!-- Product List -->
         <div class="min-h-screen px-12 sm:px-8 pb-24">
