d7d080b3070393d480cc4af66fa72ea0a23f1e01
takashi145 <t.shuhei0203@gmail.com>
2023-06-04 09:11:19 +0900

    skill list

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -4,6 +4,7 @@ interface Product {
   name: string;
   description: string | null;
   url: string | null;
+  skills: string[] | null;
 }
 
 defineProps({
@@ -12,17 +13,33 @@ defineProps({
     required: true
   }
 })
+
 </script>
 <template>
-  <div class="relative">
+  <div class="relative sm:mx-10 md:mx-0 transform transition-all duration-500 hover:scale-105">
     <img 
       :src="product.image ? product.image : '//placehold.jp/a9a9a9/ffffff/300x200.png?text=No Image' " 
-      class="block h-auto sm:h-52 lg:h-64 w-full shadow-lg max-w-full bg-gray-300 rounded-lg transition hover:scale-110 delay-150 dark:bg-slate-700 border border-gray-300 dark:border-gray-600" 
+      class="block h-auto md:h-64 lg:h-72 w-full shadow-lg max-w-full bg-gray-300 rounded-lg transition hover:scale-110 delay-150 dark:bg-slate-700 border border-gray-300 dark:border-gray-600" 
       :alt="product.name" 
     />
-    <a :href="product.url ? product.url : '#'" class="absolute inset-0 cursor-pointer bg-black bg-opacity-70 flex flex-col items-center justify-center rounded-lg hover:ring-4 opacity-0 hover:opacity-100 transition-transform duration-500 ease-in-out">
+    <a 
+      :href="product.url ? product.url : '#'" 
+      target="_blank" 
+      rel="noopener noreferrer"
+      class="absolute inset-0 cursor-pointer bg-black bg-opacity-80 flex flex-col items-center justify-center rounded-lg hover:ring-4 opacity-0 hover:opacity-100 transition-transform duration-500 ease-in-out"
+    >
       <h2 class="text-white text-center text-2xl font-semibold mb-3">{{ product.name }}</h2>
-      <p class="text-white text-center text-sm">{{ product.description }}</p>
+      <p class="mb-8 text-white text-center sm:text-xs md:text-sm px-3" style="white-space: pre-wrap;">{{ product.description }}</p>
+
+      <div class="">
+        <div 
+          v-for="(skill, index) in product.skills" 
+          :key="index" 
+          class="group relative inline-block bg-gray-200 rounded-full px-3 py-1 sm:text-xs md:text-sm font-semibold text-gray-700 mr-2 mb-2"
+        >
+          {{ skill }}
+        </div>
+      </div>
     </a>
   </div>
 </template>
---
+++ src/components/SkillCard.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import Modal from './Modal.vue';
+
+interface Skill {
+  name: string;
+  experience: string;
+  description: string;
+}
+
+const show = ref(false);
+
+defineProps({
+  skill: {
+    type: Object as () => Skill,
+    required: true,
+  }
+})
+</script>
+<template>
+  <div @click="show = true" class="w-full flex flex-col justify-between p-6 mx-auto bg-white border-2 border-gray-300 rounded-lg shadow-lg dark:bg-gray-800 transform transition-all duration-500 hover:scale-105">
+    <div class="text-2xl font-semibold text-gray-700 dark:text-white">
+        {{ skill.name }}
+    </div>
+    <div class="mt-2 text-lg text-gray-600 dark:text-gray-400">
+        {{ skill.experience }}
+    </div>
+  </div>
+
+  <Modal :show="show" @close="show = false">
+    <div class="md:flex">
+      <div class="p-8">
+        <h2 class="mb-4 uppercase tracking-wide text-xl text-indigo-500 font-semibold">
+          Skill: {{ skill.name }}
+        </h2>
+        <a href="#" class="block mt-1 text-md leading-tight font-medium text-black hover:underline dark:text-gray-200">
+          経験: 約{{ skill.experience }}
+        </a>
+        <p class="mt-2 dark:text-gray-300">{{ skill.description }}</p>
+      </div>
+    </div>
+  </Modal>
+</template>
